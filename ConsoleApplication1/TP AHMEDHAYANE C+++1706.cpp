9c753356a59f74a8d53a7805c51f9755747e3de1
ahmed hayane <hayaneahmed838@gmail.com>
2025-06-15 15:09:49 +0200

    Correction syntaxe et ajout de la surcharge de l'operateur 15/06

--- ConsoleApplication1/TP AHMEDHAYANE C+++1706.cpp
+++ ConsoleApplication1/TP AHMEDHAYANE C+++1706.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <stdexcept>
 
-// Creation de la classe d'exception pour stock insuffisant
+// creation de la classe d'exception pour stock insuffisant
 class ErreurStockInsuffisant : public std::runtime_error {
 private:
 	std::string produit;
@@ -12,11 +12,12 @@ public:
 	ErreurStockInsuffisant(const std::string& nomProduit, int qte)
 		: std::runtime_error("Plus de produit en stock à bientot :" + nomProduit), produit(nomProduit), quantite(qte) {}
 
-	std::string getProduit() const {return produit;}
-	int getQuantite() const {return quantite;}
+	std::string getTitreProduit() const {return produit;}
+	int getQuantiteDemandee() const {return quantite;}
+
 
 };
-// Creation de la classe d'exception pour argument invalide
+// creation de la classe d'exception pour argument invalide
 class ErreurArgumentInvalide : public std::invalid_argument {
 public:
 	ErreurArgumentInvalide(const std::string& message)
@@ -30,12 +31,13 @@ protected:
 	double prixBase;
 
 // constructeur
-public:                  
+protected:                  
 	Produit(std::string nom, double prix) 
 		: nomProduit(nom), prixBase(prix) {
 	}
 
-	std::string getNomproduit() const { return nomProduit; }; //accesseur
+public:
+	std::string getNomProduit() const { return nomProduit; }; //accesseur
 	double getPrixBase() const { return prixBase; };
 
 	virtual void afficherDetailsProduit() const = 0; // metho virtuel
@@ -43,8 +45,8 @@ public:
 	virtual void setStock(int nouveauStock) = 0;
 
 	virtual ~Produit() = default;
-};
 
+};
 
 // classe jeu video 
 class JeuVideo : public Produit {
@@ -52,90 +54,96 @@ private:
 	std::string genre;
 	int stock;
 
-public : 
+public:
 	JeuVideo(std::string titre, std::string genre, double prix, int stock)
 		: Produit(titre, prix), genre(genre) {
-
-		 setStock(stock);
-
-	  }
-
-	//accesseur
-	std::string getGenre() const {return genre;}
-	int getStock() const {return stock;}
-
-
-	//mutateurs
-	void setPrix(double nouveauPrix) {
-		if (nouveauPrix < 0) {
-			std::cout << "Erreur, le prix est negatif !" << std::endl;
-		}
-		prixBase = nouveauPrix;
-	}
-
-
-	void setStock(int nouveauStock) override {
-		if (nouveauStock < 0) {
-			std::cout << "Erreur, le produit est en rupture de stock" << std::endl;
-		}
-		stock = nouveauStock;
-	}
-
-	// affichage des infos sur le JV
-	void afficherInfos() const {
-		std::cout << "Titre : " << nomProduit
-			      << "Genre : " << genre
-			      << "Prix :  " << prixBase << "€"
-	       		  << "Prix TTC : " << calculerPrixTTC() << "€"
-			      << "Stock :" << stock << std::endl;
-	}
-
-	double calculerPrixTTC() const override {
-		return prixBase * 1.2;
-	}
-};
-
-// classe console
-class Console : public Produit {
-private:
-	std::string nomConsole;
-	int stock;
-
-public:
-	Console(std::string nomConsole, double prix, int stock) { // constructeur cons
-		setPrix(prix);
 		setStock(stock);
 	}
 
-	std::string getNomConsole() const { return nomConsole; }
-	double getPrix() const { return prix; }
-	int getStock() const { return stock; }
+	// accesseurs
+	std::string getGenre() const { return genre; }
+	int getStock() const  { return stock; }
 
-	//mutateurs
-	void setPrix(double nouveauPrix) {
-		if (nouveauPrix < 0) {
-			throw ErreurArgumentInvalide("le prix est negatif !");
-		} else {
-		   prix = nouveauPrix;
-		}
-	}
-
+	// mutateurs
 	void setStock(int nouveauStock) {
 		if (nouveauStock < 0) {
-			throw ErreurArgumentInvalide("le produit est en rupture de stock");
-		} else {
+			std::cout << "Erreur, le produit est en rupture de stock" << nouveauStock << std::endl;
+		}
+		else {
 			stock = nouveauStock;
 		}
 	}
-	//  affichage des infos sur la console
 
-	void afficherInfos() const {
-		std::cout << "Console :" << nomConsole
-			      << "Prix :" << prix << "€"
-			      << "Stock :" << stock << std::endl;
+	void setPrix(double nouveauPrix) {
+		if (nouveauPrix < 0) {
+			std::cout << "Erreur, le prix est négatif : " << nouveauPrix << "€" << std::endl;
+		}else {
+			prixBase = nouveauPrix;
+		}
 	}
+
+	double calculerPrixTTC() const  {
+		return prixBase * 1.2;
+	}
+
+	// affichage des infos sur le JV
+	void afficherDetailsProduit() const {
+		std::cout << "Titre : " << getNomProduit() << std::endl;
+		std::cout << "Genre : " << genre << std::endl;
+		std::cout << "Prix : " << getPrixBase() << "€" << std::endl;
+		std::cout << "Prix TTC : " << calculerPrixTTC() << "€" << std::endl;
+		std::cout << "Stock : " << stock << std::endl;
+	}
+
 };
+// classe console
+class Console : public Produit {
+private:
+	int stock;
 
+public:
+	Console(std::string nom, double prix, int stock)
+		: Produit(nom, prix) {
+		setStock(stock);
+	}
 
+	// accesseurs
+	int getStock() const { return stock; }
+
+	// mutateurs
+	void setStock(int nouveauStock) {
+		if (nouveauStock < 0) {
+			std::cout << "Erreur, le produit est en rupture de stock ! " << nouveauStock << std::endl;
+		}
+		else {
+			stock = nouveauStock;
+		}
+	}
+	void setPrix(double nouveauPrix) {
+		if (nouveauPrix < 0) {
+			std::cout << "Erreur, le prix est négatif : " << nouveauPrix << "€" << std::endl;
+		}else {
+		   prixBase = nouveauPrix;
+		}
+	}
+
+	double calculerPrixTTC() const {
+		return prixBase * 1.2;
+	}
+
+	// affichage des infos
+	void afficherDetailsProduit() const {
+		std::cout << "Nom Console : " << getNomProduit() << std::endl;
+		std::cout << "Prix : " << getPrixBase() << "€" << std::endl;
+		std::cout << "Prix TTC : " << calculerPrixTTC() << "€" << std::endl;
+		std::cout << "Stock : " << stock << std::endl;
+	}
+  };
+
+// surchage operateur classe pdt
+std::ostream& operator <<(std::ostream& os, const Produit& produit){
+	produit.afficherDetailsProduit();
+	return os;
+}
 
 
